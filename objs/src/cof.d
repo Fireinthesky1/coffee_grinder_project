f37101c4c95ef5b509f05e7f9ea8ef2d1f5f020a
Fireinthesky1 <james.hicks01234@gmail.com>
2023-11-17 13:52:15 -0800

    Restructured files build setup complete

---
+++ objs/src/cof.d
@@ -0,0 +1,6 @@
+objs/src/cof.o: src/cof.c \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h driver/inc/hw_ints.h \
+ driver/inc/hw_gpio.h driver/inc/hw_memmap.h driver/driverlib/gpio.h \
+ driver/driverlib/timer.h driver/driverlib/sysctl.h \
+ driver/driverlib/interrupt.h
---
+++ objs/sys/startup_gcc.d
@@ -0,0 +1,3 @@
+objs/sys/startup_gcc.o: sys/startup_gcc.c \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h driver/inc/hw_nvic.h \
+ driver/inc/hw_types.h
